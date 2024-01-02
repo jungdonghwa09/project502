218ad80cdd54b5292b204f25d73c51b54910820c
jungdonghwa09 <fgdgsg09@naver.com>
2024-01-02 13:55:51 +0900

    12/29

--- src/main/java/org/choongang/configs/MvcConfig.java
+++ src/main/java/org/choongang/configs/MvcConfig.java
@@ -5,14 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaAuditing
 @EnableConfigurationProperties(FileProperties.class)
 public class MvcConfig implements WebMvcConfigurer {
     @Autowired
